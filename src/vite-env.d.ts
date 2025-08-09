a6754d5150c2ed6fd51d5180ed6fe749013d8634
vanoss22 <leeweixuan39@gmail.com>
2025-08-09 17:41:51 +0800

    feat: IT WORKS MDFK

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BACKEND_URL: string
+  readonly VITE_GOOGLE_CLIENT_ID: string
+  readonly VITE_FACEBOOK_CLIENT_ID: string
+  readonly VITE_ENOKI_API_KEY: string
+  readonly VITE_FULLNODE_URL: string
+  readonly VITE_ESCROW_PACKAGE_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
